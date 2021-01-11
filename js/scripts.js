6ff22ea950f61f5f26dc0028395f1f86f4491988
Scott O'Neil <scottieoneil@gmail.com>
2021-01-11 13:02:15 -0800

    add fnct to count index

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@ $(document).ready(function() {
     const sentence = prompt("Please enter a sentence:");
     const sent = sentence.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
 
-    function reverseStr(str) {
-        return str.split().reverse().join("");
+    function reverseString(str) {
+        return str.split("").reverse().join("");
     }
-    sent2 = reverseStr(sent);
-    console.log(sent2);
+    let countWords = function(sentence) {
+        return sentence.split('').length;
+    }
+
+    let sent2 = reverseString(sent);
+    let sent3 = sentence + sent2;
+    let sent4 = Math.floor(countWords(sentence) / 2)
+    let sent5 = sentence.charAt(sent4) + sentence + sent2
+    console.log(sent5);
 });
