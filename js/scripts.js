3b0aee4aaa5ec774288863571b8eee83898c5713
Scott O'Neil <scottieoneil@gmail.com>
2021-01-11 11:23:54 -0800

    add function for 1st and last char

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     const sentence = prompt("Please enter a sentence:");
-    alert(sentence);
+    const sent = sentence.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
+
+    function reverseStr(str) {
+        return str.split().reverse().join("");
+    }
+    sent2 = reverseStr(sent);
+    console.log(sent2);
 });
